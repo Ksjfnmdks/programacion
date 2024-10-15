f028c2c7422c9e99db08bbbd2b28d1e2216bc4ce
Kevin <mezagiraldokevin1@gmail.com>
2024-10-15 08:41:06 -0500

    validacion en los campos del crear usuario

--- views/usuario/create.php
+++ views/usuario/create.php
@@ -2,18 +2,21 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
-use app\models\Roles;
-use app\models\Estados;
+use app\models\TblRoles;
+use app\models\TblEstados;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Usuarios */
 /* @var $form yii\widgets\ActiveForm */
 
+
 $this->registerCssFile("@web/css/UsuariosForm.css", ['depends' => [yii\web\YiiAsset::className()]]);
 ?>
 <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="UsuariosForm.css">
 
 <div class="containerUsu">
+    
     <?php if (Yii::$app->session->hasFlash('success')): ?>
         <div class="alert alert-success">
             <?= Yii::$app->session->getFlash('success') ?>
@@ -21,7 +24,7 @@ $this->registerCssFile("@web/css/UsuariosForm.css", ['depends' => [yii\web\YiiAs
     <?php endif; ?>
 
     <div class="titulo">
-        <h1>Crear Usuarios</h1>
+        <h1>Crear Usuario</h1>
     </div>
     <hr class="divider">
     <div class="lista">
@@ -36,38 +39,43 @@ $this->registerCssFile("@web/css/UsuariosForm.css", ['depends' => [yii\web\YiiAs
             ['class' => 'listausu']
         ) ?>
     </div>
+    
+    <div class="UsuariosForm">
 
-        <div class="UsuariosForm">
         <?php $form = ActiveForm::begin(); ?>
+
         <div class="form-grid">
-            
-            <?= $form->field($model, 'identificacion')->textInput(['maxlength' => true, 'placeholder' => 'Identificación']) ?>
 
-            <?= $form->field($model, 'nombre')->textInput(['maxlength' => true, 'placeholder' => 'Nombres']) ?>
+            <?= $form->field($model, 'identificacion')->textInput(['maxlength' => 10, 'placeholder' => 'Identificación', 'oninput' => "this.value = this.value.replace(/[^0-9]/g, '')"]) //oninput para que solo acepte numeros en este campo?> 
 
-            <?= $form->field($model, 'apellido')->textInput(['maxlength' => true, 'placeholder' => 'Apellidos']) ?>
+            <?= $form->field($model, 'nombre')->textInput(['maxlength' => 30, 'placeholder' => 'Nombres', 'oninput' => "this.value = this.value.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g, '')"]) //oninput para que solo acepte letras en este campo?>
 
-            <?= $form->field($model, 'telefono')->textInput(['maxlength' => true, 'placeholder' => 'Teléfono']) ?>
+            <?= $form->field($model, 'apellido')->textInput(['maxlength' => 30, 'placeholder' => 'Apellidos', 'oninput' => "this.value = this.value.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g, '')"]) ?>
 
-            <?= $form->field($model, 'correo')->input('email', ['placeholder' => 'Correo Electrónico']) ?>
+            <?= $form->field($model, 'telefono')->textInput(['maxlength' => 10, 'placeholder' => 'Teléfono', 'oninput' => "this.value = this.value.replace(/[^0-9]/g, '')"]) ?>
 
-            <?= $form->field($model, 'username')->textInput(['maxlength' => true, 'placeholder' => 'Usuario']) ?>
+            <?= $form->field($model, 'correo')->input('email', ['maxlength' => 100,'placeholder' => 'Correo Electrónico']) ?>
 
-            <?= $form->field($model, 'password')->passwordInput(['maxlength' => true, 'placeholder' => 'Contraseña']) ?>
+            <?= $form->field($model, 'username')->textInput(['maxlength' => 30, 'placeholder' => 'Usuario']) ?>
+
+            <?= $form->field($model, 'password')->passwordInput(['maxlength' => 50, 'placeholder' => 'Contraseña']) ?>
 
             <?= $form->field($model, 'rol_id_FK')->dropDownList(
-                ArrayHelper::map(Roles::find()->all(), 'rol_id', 'nombre'), 
+                ArrayHelper::map(TblRoles::find()->all(), 'rol_id', 'nombre'), 
                 ['prompt' => 'Seleccione un Rol']
             ) ?>
 
             <?= $form->field($model, 'est_id_FK')->dropDownList(
-                ArrayHelper::map(Estados::find()->all(), 'est_id', 'descripcion'), 
+                ArrayHelper::map(TblEstados::find()->all(), 'est_id', 'descripcion'), 
                 ['prompt' => 'Seleccione un Estado']
             ) ?>
+
         </div>
-            <div class="boton">
-                <?= Html::submitButton('Registrar', ['class' => 'btn-registrar']) ?>
-            </div>
-            <?php ActiveForm::end(); ?>
     </div>
+    
+        <div class="boton">
+            <?= Html::submitButton('Registrar', ['class' => 'btn-registrar']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
 </div>
